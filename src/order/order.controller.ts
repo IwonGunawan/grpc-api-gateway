5e7d753417816483fba1a081d5c8ff6a6be23de6
Iwon <iwongunawan@gmail.com>
2022-09-26 15:27:20 +0700

    module order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, OnModuleInit, Inject, Post, UseGuards, Req } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from "rxjs";
+import { Request } from "express";
+import { CreateOrderRequest, CreateOrderResponse, OrderServiceClient, ORDER_SERVICE_NAME } from './order.pb';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController implements OnModuleInit {
+    private svc: OrderServiceClient;
+
+    @Inject(ORDER_SERVICE_NAME)
+    private readonly client: ClientGrpc;
+
+    public onModuleInit():void {
+        this.svc = this.client.getService<OrderServiceClient>(ORDER_SERVICE_NAME)
+    }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    private async createOrder(@Req() req: Request): Promise<Observable<CreateOrderResponse>>{
+        const body: CreateOrderRequest = req.body
+        
+        //body.userId = <number>req.user
+        body.userId = 1
+
+        return this.svc.createOrder(body)
+    }
+}
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { OrderController } from './order.controller';
+import { ORDER_PACKAGE_NAME, ORDER_SERVICE_NAME } from './order.pb';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name : ORDER_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50052',
+          package: ORDER_PACKAGE_NAME,
+          protoPath: 'node_modules/grpc-proto/proto/order.proto'
+        }
+      }
+    ])
+  ],
   controllers: [OrderController]
 })
 export class OrderModule {}
